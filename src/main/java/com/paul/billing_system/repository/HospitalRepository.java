020168fc44c9d9866fdf5b97dc360afe0f3ce00a
Paul Tamal <94731937+paultomal@users.noreply.github.com>
2023-08-27 17:33:27 +0600

    Hospital entity deleted

--- src/main/java/com/paul/billing_system/repository/HospitalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.paul.billing_system.repository;
-
-import com.paul.billing_system.entity.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-}
