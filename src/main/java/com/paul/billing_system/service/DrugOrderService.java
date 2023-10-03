a2ec47411cff0e075995d676df86475064c01b11
ImamBiplob <imamhbiplob@gmail.com>
2023-10-03 11:10:18 +0600

    Done with checking & updating quantity, and set total according to available drugs

--- src/main/java/com/paul/billing_system/service/DrugOrderService.java
+++ src/main/java/com/paul/billing_system/service/DrugOrderService.java
@@ -1,16 +1,16 @@
 package com.paul.billing_system.service;
 
+import com.paul.billing_system.dto.DrugDTO;
 import com.paul.billing_system.dto.DrugOrderDTO;
 import com.paul.billing_system.entity.DrugOrder;
+import com.paul.billing_system.entity.OrgDrugPriceQuantity;
 import com.paul.billing_system.entity.Patient;
-import com.paul.billing_system.repository.DrugOrderRepository;
-import com.paul.billing_system.repository.DrugRepository;
-import com.paul.billing_system.repository.OrganizationRepository;
-import com.paul.billing_system.repository.PatientRepository;
+import com.paul.billing_system.repository.*;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,12 +19,14 @@ public class DrugOrderService {
     private final OrganizationRepository organizationRepository;
     private final DrugRepository drugRepository;
     private final PatientRepository patientRepository;
+    private final OrgDrugPriceQuantityRepository priceQuantityRepository;
 
-    public DrugOrderService(DrugOrderRepository drugOrderRepository, OrganizationRepository organizationRepository, DrugRepository drugRepository, PatientRepository patientRepository) {
+    public DrugOrderService(DrugOrderRepository drugOrderRepository, OrganizationRepository organizationRepository, DrugRepository drugRepository, PatientRepository patientRepository, OrgDrugPriceQuantityRepository priceQuantityRepository) {
         this.drugOrderRepository = drugOrderRepository;
         this.organizationRepository = organizationRepository;
         this.drugRepository = drugRepository;
         this.patientRepository = patientRepository;
+        this.priceQuantityRepository = priceQuantityRepository;
     }
 
     @Transactional
@@ -44,11 +46,27 @@ public class DrugOrderService {
         }
 
         drugOrder.setOrganization(organizationRepository.findById(drugOrderDTO.getOrgId()).orElseThrow());
-        drugOrder.setDrugList(drugOrderDTO.getDrugList()
+
+        List<DrugDTO> availableDrugs = new ArrayList<>();
+        drugOrderDTO.getDrugList().stream().peek(d -> {
+            OrgDrugPriceQuantity drugPriceQuantity = priceQuantityRepository.findByOrganizationIdAndDrugId(drugOrderDTO.getOrgId(), d.getId());
+            if(drugPriceQuantity != null) {
+                if(drugPriceQuantity.getQuantity() != null && drugPriceQuantity.getQuantity() > 0) {
+                    availableDrugs.add(d);
+                    drugPriceQuantity.setQuantity(drugPriceQuantity.getQuantity() - 1);
+                }
+            }
+        });
+
+        List<Double> priceList = availableDrugs.stream().map(DrugDTO::getPrice).toList();
+        Double total = priceList.stream().reduce(0.0, Double::sum);
+
+        drugOrder.setDrugList(availableDrugs
                 .stream()
                 .map(drugDTO -> drugRepository.findById(drugDTO.getId()).orElse(null))
                 .toList());
-        drugOrder.setTotal(drugOrderDTO.getTotal());
+
+        drugOrder.setTotal(total);
 
         return DrugOrderDTO.form(drugOrderRepository.save(drugOrder));
     }
